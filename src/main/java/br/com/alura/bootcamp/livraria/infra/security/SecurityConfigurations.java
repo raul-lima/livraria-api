9cda7aa3e6c1258309e0f6bf96cf1ae51505ed91
Raul <raultorres.lima@gmail.com>
2021-11-12 16:50:22 -0300

    configura deploy automatico

--- src/main/java/br/com/alura/bootcamp/livraria/infra/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/bootcamp/livraria/infra/security/SecurityConfigurations.java
@@ -32,8 +32,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
 
     @Override
     @Bean
-    protected AuthenticationManager authenticationManager() throws Exception {
-        return super.authenticationManager();
+    public AuthenticationManager authenticationManagerBean() throws Exception {
+        return super.authenticationManagerBean();
     }
 
     @Override
@@ -51,6 +51,7 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
         http
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, "/auth").permitAll() // Autoriza acessar o /auth sem precisar autenticar antes. Ou seja, não precisa estar logado para tentar logar
+                .antMatchers(HttpMethod.GET, "/home").permitAll()
                 .antMatchers("/usuarios/**").hasRole("ADMIN")
                 .anyRequest().authenticated() // qualquer requisição precisa estar logado
                 .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //indica pro string que o gerenciamento de sessão é stateless. Cria a necessidade de fazer um controller de autenticação, porque não está mais usando o do spring
